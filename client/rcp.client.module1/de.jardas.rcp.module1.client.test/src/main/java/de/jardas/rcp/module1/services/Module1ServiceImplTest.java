bd4bc7096b7398762004e025d3c154c3a2539197
Alexander Gordt <alexander.gordt@tntinnight.de>
2014-03-10 09:13:14 +0100

    Working build

--- client/rcp.client.module1/de.jardas.rcp.module1.client.test/src/main/java/de/jardas/rcp/module1/services/Module1ServiceImplTest.java
+++ client/rcp.client.module1/de.jardas.rcp.module1.client.test/src/main/java/de/jardas/rcp/module1/services/Module1ServiceImplTest.java
@@ -2,13 +2,15 @@ package de.jardas.rcp.module1.services;
 
 import org.junit.Test;
 
-
+import de.jardas.rcp.module1.client.pub.SharedClientInterface;
 
 public class Module1ServiceImplTest {
 
+	private SharedClientInterface clientService;
+
 	@Test
-    public void basic() {
-		
+	public void basic() {
+		clientService.doSomething();
 	}
-	
+
 }
